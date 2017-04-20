daf612c3a1ba7895e7ecdfebe7401b515509bb10
lyxia <lysxia@gmail.com>
2017-04-20 16:37:59 -0400

    Small step reduction

--- src/Decor/Soup/Tree.hs
+++ src/Decor/Soup/Tree.hs
@@ -33,3 +33,43 @@ treeDCore s t = do
     Pi rel () t1 t2 -> Pi rel () <$> treeDCore s t1 <*> treeDCore s t2
     Abs rel () t1 t2 -> Abs rel () <$> treeDCore s t1 <*> treeDCore s t2
     App b a rel -> App <$> treeDCore s b <*> treeDCore s a <*> pure rel
+
+step :: DCore Tree -> Maybe (DCore Tree)
+step Star = Nothing
+step (Fun _) = Nothing
+step (Var _) = Nothing
+step (Pi _ _ _ _) = Nothing
+step (Abs Irr () t a) = Abs Irr () t <$> step a
+step (App b a rel) =
+  case (b, step b) of
+    (_, Just b) -> Just (App b a rel)
+    (Abs rel' () _ b, Nothing) | rel == rel' -> Just (sub a b)
+    _ -> Nothing
+
+sub :: DCore Tree -> DCore Tree -> DCore Tree
+sub = sub' (DeBruijnV 0)
+
+sub' :: DeBruijnV -> DCore Tree -> DCore Tree -> DCore Tree
+sub' n a b = case b of
+  Star -> Star
+  Fun f -> Fun f
+  Var v | v == n -> shiftTerm n a
+        | v < n -> Var v
+        | otherwise -> Var (shift v (-1))
+  Pi rel () t1 t2 -> Pi rel () (sub' n a t1) (sub' (shift n 1) a t2)
+  Abs rel () t1 t2 -> Abs rel () (sub' n a t1) (sub' (shift n 1) a t2)
+  App t1 t2 rel -> App (sub' n a t1) (sub' n a t2) rel
+
+shiftTerm :: DeBruijnV -> DCore Tree -> DCore Tree
+shiftTerm (DeBruijnV n) a = shiftTerm' n (DeBruijnV 0) a
+
+shiftTerm' :: Shift -> DeBruijnV -> DCore Tree -> DCore Tree
+shiftTerm' n m a = case a of
+  Star -> Star
+  Fun f -> Fun f
+  Var v
+    | v < m -> Var v
+    | otherwise -> Var (shift v n)
+  Pi rel () t1 t2 -> Pi rel () (shiftTerm' n m t1) (shiftTerm' n (shift m 1) t2)
+  Abs rel () t1 t2 -> Abs rel () (shiftTerm' n m t1) (shiftTerm' n (shift m 1) t2)
+  App t1 t2 rel -> App (shiftTerm' n m t1) (shiftTerm' n m t2) rel
